8e7abe8dca98da5605becd9edf1616667bf071b7
Hentioe <meow.i5.br@gmail.com>
2022-12-16 08:16:40 +0800

    Update erlang 24+

--- asdf/erlang-24+.nix
+++ asdf/erlang-24+.nix
@@ -12,12 +12,7 @@ mkShell {
     libxml2
     libGL
     libGLU
-    # 从处不能使用 wxGTK31，具体细节和 wxGTK 包的默认版本有关。
-    # 详情：https://github.com/NixOS/nixpkgs/issues/63579
-    (wxGTK30.override {
-      withWebKit = true;
-      withGtk2 = false;
-    })
+    (wxGTK32.override { withWebKit = true; })
     xorg.libX11
   ];
 
