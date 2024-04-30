66c482f1d402da26c315c2e771a9cd2af89e7bbf
Hentioe <meow.i5.br@gmail.com>
2024-04-30 13:56:05 +0800

    Add mise nix-shells

---
+++ mise/erlang-24+.nix
@@ -0,0 +1,25 @@
+with import <nixpkgs> { };
+
+mkShell {
+  buildInputs = [
+    pkg-config
+    gnumake
+    autoconf
+    ncurses
+    openssl
+    libxslt
+    fop
+    libxml2
+    libGL
+    libGLU
+    (wxGTK32.override { withWebKit = true; })
+    xorg.libX11
+  ];
+
+  shellHook = ''
+    export KERL_CONFIGURE_OPTIONS="--with-ssl=${
+      lib.getOutput "out" openssl
+    } --with-ssl-incl=${lib.getDev openssl} --without-javac --without-odbc";
+    export KERL_BUILD_DOCS=yes;
+  '';
+}
